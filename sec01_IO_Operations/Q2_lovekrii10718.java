c5bf3846d8ef76c468d80ed87eaf8fa0128438c6
Heesom <heesom.hs@gmail.com>
2022-09-28 18:43:47 +0900

    Revised Entire Format

--- sec01_IO_Operations/Q2_lovekrii10718.java
+++
@@ -1,9 +0,0 @@
-// https://www.acmicpc.net/problem/10718
-
-package sec01_IO_Operations;
-
-class Q2_lovekrii10718{
-    public static void main(String[] args){
-        System.out.println("강한친구 대한육군 강한친구 대한육군");
-    }
-}
