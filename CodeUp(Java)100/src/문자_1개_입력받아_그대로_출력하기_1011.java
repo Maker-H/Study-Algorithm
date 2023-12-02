bd69dc61116c930928e840717a2e5c2006f55583
Heesom <heesom.hs@gmail.com>
2023-12-02 12:25:36 +0900

    [1011] 문자 1개 입력받아 그대로 출력하기
    https://codeup.kr/problem.php?id=1011

---
+++ CodeUp(Java)100/src/문자_1개_입력받아_그대로_출력하기_1011.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 문자_1개_입력받아_그대로_출력하기_1011 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+
+        System.out.println(next);
+    }
+}
