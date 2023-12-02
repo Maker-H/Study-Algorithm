43ebc845d9ba789747c33e2d3a97f465b43c06bd
Heesom <heesom.hs@gmail.com>
2023-12-02 12:22:52 +0900

    [1010] 정수 1개 입력받아 그대로 출력하기
    https://codeup.kr/problem.php?id=1010

---
+++ CodeUp(Java)100/src/정수_1개_입력받아_그대로_출력하기_1010.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 정수_1개_입력받아_그대로_출력하기_1010 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        System.out.println(i);
+    }
+}
