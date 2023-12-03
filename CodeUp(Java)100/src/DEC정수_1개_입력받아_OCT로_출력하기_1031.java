9025ca0ded689c00bda0a0a2080cac23f78c5d25
Heesom <heesom.hs@gmail.com>
2023-12-03 12:37:43 +0900

    [1031] DEC정수 1개 입력받아 OCT로 출력하기 (%o 표현)
    https://codeup.kr/problem.php?id=1031

---
+++ CodeUp(Java)100/src/DEC정수_1개_입력받아_OCT로_출력하기_1031.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DEC정수_1개_입력받아_OCT로_출력하기_1031 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.printf("%o", i);
+    }
+}
+
