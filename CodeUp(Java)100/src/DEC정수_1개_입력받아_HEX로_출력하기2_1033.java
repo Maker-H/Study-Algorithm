608aa26636b75bcf22a009bd583930d0564b99b7
Heesom <heesom.hs@gmail.com>
2023-12-03 12:39:14 +0900

    [1033] DEC정수 1개 입력받아 HEX로(대문자) 출력하기 (%X, %x 표현)
    https://codeup.kr/problem.php?id=1033

---
+++ CodeUp(Java)100/src/DEC정수_1개_입력받아_HEX로_출력하기2_1033.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DEC정수_1개_입력받아_HEX로_출력하기2_1033 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.printf("%X", i);
+    }
+}
+
