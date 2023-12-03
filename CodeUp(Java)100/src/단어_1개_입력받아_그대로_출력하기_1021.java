8062be3da6e2919882e6ccce3aab9ad30e277dfe
Heesom <heesom.hs@gmail.com>
2023-12-03 12:07:58 +0900

    [1021] 단어 1개 입력받아 그대로 출력하기
    https://codeup.kr/problem.php?id=1021

---
+++ CodeUp(Java)100/src/단어_1개_입력받아_그대로_출력하기_1021.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class 단어_1개_입력받아_그대로_출력하기_1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        System.out.println(word);
+    }
+}
