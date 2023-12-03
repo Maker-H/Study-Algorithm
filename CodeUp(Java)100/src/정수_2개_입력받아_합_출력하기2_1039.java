412e4bff49d76ca5e9e63372ba6f114d9e357a1a
Heesom <heesom.hs@gmail.com>
2023-12-03 17:28:15 +0900

    [1039] 정수 2개 입력받아 합 출력하기2
    https://codeup.kr/problem.php?id=1039

---
+++ CodeUp(Java)100/src/정수_2개_입력받아_합_출력하기2_1039.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class 정수_2개_입력받아_합_출력하기2_1039 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num1 = scanner.nextLong();
+        long num2 = scanner.nextLong();
+
+        System.out.println(num1 + num2);
+    }
+}
+
