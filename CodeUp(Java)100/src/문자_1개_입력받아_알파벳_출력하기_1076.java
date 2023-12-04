69bc180b337c506a67c2fe50d4a5ac9a5bd799b2
Heesom <heesom.hs@gmail.com>
2023-12-04 13:46:17 +0900

    [1076] 문자 1개 입력받아 알파벳 출력하기
    https://codeup.kr/problem.php?id=1076

---
+++ CodeUp(Java)100/src/문자_1개_입력받아_알파벳_출력하기_1076.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class 문자_1개_입력받아_알파벳_출력하기_1076 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+
+        for (int i = (int) 'a'; i <= (int) c; i++) {
+            System.out.println((char) i);
+        }
+    }
+}
