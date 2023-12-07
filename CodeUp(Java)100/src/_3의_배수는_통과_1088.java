53de837d96034abc11635e7901dccf421bf74489
Heesom <heesom.hs@gmail.com>
2023-12-07 21:27:54 +0900

    [1088] 3의 배수는 통과
    https://codeup.kr/problem.php?id=1088

---
+++ CodeUp(Java)100/src/_3의_배수는_통과_1088.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _3의_배수는_통과_1088 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        for (int i = 1; i <= input; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
+
