68737e0ffe91c433ffe5f499775d5ae8a3b0efab
Heesom <heesom.hs@gmail.com>
2023-12-07 18:27:15 +0900

    [1082] HEX 구구단
    https://codeup.kr/problem.php?id=1082

---
+++ CodeUp(Java)100/src/HEX_구구단_1082.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HEX_구구단_1082 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int input = Integer.parseInt(s, 16);
+
+        for (int i = 1; i < 16; i++) {
+            System.out.printf("%X*%X=%X\n",input, i, i * input);
+        }
+    }
+}
+
