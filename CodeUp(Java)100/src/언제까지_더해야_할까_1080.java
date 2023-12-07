1d9e97a90d4004a1537808f0c8df4be7ac9409e8
Heesom <heesom.hs@gmail.com>
2023-12-07 18:20:06 +0900

    [1080] 언제까지 더해야 할까
    https://codeup.kr/problem.php?id=1080

---
+++ CodeUp(Java)100/src/언제까지_더해야_할까_1080.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class 언제까지_더해야_할까_1080 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        int total = 0;
+        int start = 1;
+        while (total < input) {
+            total += start;
+            start++;
+        }
+
+        System.out.println(start - 1);
+    }
+}
+
