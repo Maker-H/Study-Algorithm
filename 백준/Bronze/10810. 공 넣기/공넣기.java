9aa479f8ba08a5d794c1a440b87bca51d5bcb4da
Heesom <heesom.hs@gmail.com>
2023-12-09 11:44:12 +0900

    [Bronze III] Title: 공 넣기 (ArrayList stream으로 초기화 방법)
    - IntStream.generate(()->0).limit().boxed.collect(Collectors.toCollection(ArrayList::new);

---
+++ 백준/Bronze/10810. 공 넣기/공넣기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class 공넣기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] numbers = br.readLine().split(" ");
+        int N = Integer.parseInt(numbers[0]);
+        int M = Integer.parseInt(numbers[1]);
+
+
+        List<Integer> bags = IntStream.generate(() -> 0)
+                .limit(N)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        for (int z = 0; z < M; z++) {
+            String[] input = br.readLine().split(" ");
+            int i = Integer.parseInt(input[0]) - 1;
+            int j = Integer.parseInt(input[1]) - 1;
+            int k = Integer.parseInt(input[2]);
+            for (int a = i; a <= j; a++) {
+                bags.set(a, k);
+            }
+        }
+
+        bw.write(String.join(" ", bags.stream().map(Object::toString).toArray(String[]::new)));
+        bw.flush();
+    }
+
+}
+
