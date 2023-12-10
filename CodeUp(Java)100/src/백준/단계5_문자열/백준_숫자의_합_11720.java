dff0354848edc93a51bf035d60a4b847a2ba582a
Heesom <heesom.hs@gmail.com>
2023-12-10 12:17:28 +0900

    [11720] 숫자의 합
    https://www.acmicpc.net/problem/11720

---
+++ CodeUp(Java)100/src/백준/단계5_문자열/백준_숫자의_합_11720.java
@@ -0,0 +1,21 @@
+package 백준.단계5_문자열;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class 백준_숫자의_합_11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = Arrays.stream(br.readLine().split(""))
+                .mapToInt(Integer::parseInt)
+                .sum();
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+
+}
+
