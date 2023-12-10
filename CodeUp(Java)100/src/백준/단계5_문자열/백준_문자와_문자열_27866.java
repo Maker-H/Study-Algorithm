23000b5c5de2660f7b55fc010600db97fb99f3a9
Heesom <heesom.hs@gmail.com>
2023-12-10 12:06:28 +0900

    [27866] 문자와 문자열
    https://www.acmicpc.net/problem/27866

--- CodeUp(Java)100/src/백준/단계5_문자열/백준_문자와_문자열_27866.java
+++ CodeUp(Java)100/src/백준/단계5_문자열/백준_문자와_문자열_27866.java
@@ -11,31 +11,12 @@ public class 백준_문자와_문자열_27866 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> scores = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt) // IntStream이 됨
-                .boxed() // Collector는 참조형 객체만 다룸
-                .collect(Collectors.toCollection(ArrayList::new));
-        Collections.sort(scores, Collections.reverseOrder());
+        String[] S = br.readLine().split("");
+        int i = Integer.parseInt(br.readLine());
 
-        double M = scores.get(0);
-
-        double total = scores.stream().mapToDouble(n -> n / M * 100).sum();
-
-        bw.write(String.valueOf(total / N));
-
-
+        bw.write(S[i - 1]);
         bw.flush();
     }
 
 }
 
-
-/*
-ArrayList<Integer> list = new ArrayList<Integer>();
-Collections.sort(list); // 오름차순
-Collections.sort(list, Collections.reverseOrder()); // 내림차순
-
-<stream 사용해서 새로운 배열 생성>
-List<Integer> sortedList = list.stream().sorted().collect(Collectors.toList());
- */
