42b161cb67224fe7a7b96fdbd0f9f055e253d878
Heesom <heesom.hs@gmail.com>
2023-12-10 12:13:55 +0900

    [11654] 아스키 코드
    https://www.acmicpc.net/problem/11654

---
+++ CodeUp(Java)100/src/백준/단계5_문자열/백준_아스키코드_11654.java
@@ -0,0 +1,17 @@
+package 백준.단계5_문자열;
+
+import java.io.*;
+
+public class 백준_아스키코드_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = (int) br.readLine().charAt(0);
+
+        bw.write(String.valueOf(T));
+        bw.flush();
+    }
+
+}
+
