c5f14ee51cafb4e15ebdf18c0411036e5740e131
Heesom <heesom.hs@gmail.com>
2023-12-11 13:42:17 +0900

    [25083] 새싹 (\ " 의 경우 \\ \" 표시)
    https://www.acmicp.net/problem/25083

---
+++ CodeUp(Java)100/src/백준/단계6_심화1/백준_새싹_25083.java
@@ -0,0 +1,21 @@
+package 백준.단계6_심화1;
+
+import java.io.*;
+
+public class 백준_새싹_25083 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        bw.write(input);
+        bw.flush();
+    }
+
+}
+
