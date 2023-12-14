f9daeb765c600ef682ffdc78d48dee34c76ef183
Heesom <heesom.hs@gmail.com>
2023-12-14 16:37:51 +0900

    [11005] 진법 변환2 (10진법->n진법)
    - Integer.toString(10진수, 바꿀 n)
    
    https://codeup.kr/problem.php?id=11005

---
+++ 백준(개인)/src/백준/단계8_일반수학1/백준_진법변환2_11005.java
@@ -0,0 +1,18 @@
+package 백준.단계8_일반수학1;
+
+import java.io.*;
+
+public class 백준_진법변환2_11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+
+        String ans = Integer.toString(Integer.parseInt(input[0]), Integer.parseInt(input[1])).toUpperCase();
+
+        bw.write(ans);
+
+        bw.flush();
+    }
+}
