d640e90f7a14267f676971b9d0ea59d2cc6863fe
Heesom <heesom.hs@gmail.com>
2023-12-15 16:08:28 +0900

    [27323] 직사각형
    https://codeup.kr/problem.php?id=27323

---
+++ 백준(개인)/src/백준/단계10_기하_직각사각형과_삼각형/백준_직사각형_27323.java
@@ -0,0 +1,17 @@
+package 백준.단계10_기하_직각사각형과_삼각형;
+
+import java.io.*;
+
+public class 백준_직사각형_27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A * B);
+
+        bw.flush();
+    }
+}
