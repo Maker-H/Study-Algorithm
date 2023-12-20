2048b71b52df598ed85eaded792db69e66bdd0b1
Heesom <heesom.hs@gmail.com>
2023-12-20 14:26:34 +0900

    [파일] 디렉터리 수정

---
+++ 백준(개인)/src/백준/알고리즘_기초/브루트포스/순열/백준_.java
@@ -0,0 +1,17 @@
+package 백준.알고리즘_기초.브루트포스.순열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(sb);
+
+    }
+
+}
+
